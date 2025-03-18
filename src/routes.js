8723a782f2711a0673b4da11935ba0e9cf5d93ad
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-18 11:16:03 -0300

    New routes to query db

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  return res.json({ hello: "world" });
-});
+const userController = require("./controllers/UserController");
+
+router.get("/users", userController.index);
+router.post("/users", userController.store);
 
 module.exports = router;
